0c9f90836583d54eaedfabc7a0533a1925ea09f9
Kamdem Lens <nowru@learndobe.com>
2019-09-19 11:00:39 +0100

    Started slider code for projects & testimonies

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
+$(document).ready(function(){
+  var slideIndex = 0;
 
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("sl-item");
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+
+
+
+  $('.sl-item').ready(function() {
+
+  });
+});
 
